3850a08ee54593d332243f708e57246235bf4592
Alexander-Drumev <Aleksander.Drumev@waveaccess.global>
2022-06-15 13:00:09 +0300

    Making the forgotten basket notification one-time

--- WA.Pizza.Web/BackgroundJobs/ForgottenBasketsJob.cs
+++ WA.Pizza.Web/BackgroundJobs/ForgottenBasketsJob.cs
@@ -21,13 +21,21 @@ namespace WA.Pizza.Web.BackgroundJobs
 			var usersWhoForgotBasket = await _dbContext
 				.Baskets
 				.Include(b => b.BasketItems)
+				.Include(b => b.User)
 				.Where(b => b.BasketItems.Any() &&
 							b.LastModifiedOn.HasValue &&
 							DateTime.UtcNow.Subtract(b.LastModifiedOn.Value) >= TimeSpan.FromDays(7))
-				.Select(b => b.UserId.ToString() ?? "Anonymous user")
+				.Select(b => b.User)
 				.ToListAsync();
 
-			_logger.LogInformation($"Users: { string.Join(", ", usersWhoForgotBasket) } have forgottern their basket.");
+			_logger.LogInformation($"Users: { string.Join(", ", usersWhoForgotBasket.Select(u => u.UserName ?? "Anonymous user")) } have forgottern their basket.");
+
+			foreach (var user in usersWhoForgotBasket)
+			{
+				user.Basket.LastModifiedOn = null;
+			}
+
+			await _dbContext.SaveChangesAsync();
 		}
 	}
 }
