7786b1ae25e1f6fc20e99bdc1460f2389e62c821
Alexander-Drumev <Aleksander.Drumev@waveaccess.global>
2022-07-06 10:15:36 +0300

    Specification pattern applied

---
+++ WA.Pizza.Infrastructure/Abstractions/BasketNotModifiedForWeekSpecification.cs
@@ -0,0 +1,15 @@
+﻿using WA.Pizza.Core.Abstractions;
+using WA.Pizza.Core.Models;
+
+namespace WA.Pizza.Infrastructure.Abstractions
+{
+	public class BasketNotModifiedForWeekSpecification : ExpressionSpecification<Basket>
+	{
+		public BasketNotModifiedForWeekSpecification() : base(basket => basket.BasketItems.Any() &&
+															  basket.LastModifiedOn.HasValue &&
+															  DateTime.UtcNow.Subtract(basket.LastModifiedOn.Value) >= TimeSpan.FromDays(7))
+		{
+
+		}
+	}
+}
