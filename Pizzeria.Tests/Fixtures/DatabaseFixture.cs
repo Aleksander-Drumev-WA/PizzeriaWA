c28e941a14a234cbaf5b2c16800ef2365689f66a
Alexander-Drumev <Aleksander.Drumev@waveaccess.global>
2022-07-12 12:33:12 +0300

    removed part of the connection string in test fixture

--- Pizzeria.Tests/Fixtures/DatabaseFixture.cs
+++ Pizzeria.Tests/Fixtures/DatabaseFixture.cs
@@ -22,7 +22,7 @@ namespace Pizzeria.Tests.Fixtures
 
         public DatabaseFixture()
         {
-            _connection = new SqlConnection("Server=localhost,5121;Database=WA.PizzaDB.Tests;User=sa;Password=#sql-pass22_;TrustServerCertificate=True");
+            _connection = new SqlConnection("Server=localhost,5121;Database=WA.PizzaDB.Tests;User=sa;Password=#sql-pass22_");
             _options = new DbContextOptionsBuilder<AppDbContext>().UseSqlServer(_connection.ConnectionString).Options;
             DbContext = new AppDbContext(_options);
             DbContext.Database.Migrate();
